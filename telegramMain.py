f2b13d1ce7d45eb3c3bfc50a3d610f8ce8ae0d50
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2022-12-12 21:49:26 -0300

    Calculo rendimento

--- telegramMain.py
+++ telegramMain.py
@@ -1,21 +1,34 @@
 import json
 import logging
-from typing import Self
 from telegram import*
 from telegram.ext import*
 import responses as R
+
+async def start(update: Update, context: CallbackContext) -> None:
+    update.messagem.reply_text("Olá!")
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
 async def estimativa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Vamos fazer uma estimativa sobre investimentos de acondo com as informações dadas, para isso digite /setcommands')
+    await update.message.reply_text(f'Vamos fazer uma estimativa sobre investimentos de acordo com as informações dadas, para isso selecione o tempo para manter seu dinheiro investido: \n /seisMeses \n umAno \n quatroAnos \n /trintaAnos')
 
 
 def tempInvest(bot, update):
   bot.message.reply_text(main_menu_message(),
                          reply_markup=main_menu_keyboard())
+
+
+async def seisMeses(update, context):
+    update.message.reply_text(f'Simule um valor a investir todo mês: ')
+    text = int(context.args[0])
+    valorEstimado = (((text *1,16) + text) * 1,16)
+    update.message.reply_text('Valor em seis meses:' + valorEstimado )
+
+
+
+#MENU
 
 def main_menu(bot, update):
     bot.callback_query.message.edit_text(main_menu_message(),
@@ -28,15 +41,9 @@ def main_menu_keyboard():
     return InlineKeyboardMarkup(keyboard)
 
 def main_menu_message():
-  return 'Escolha o tempo determinada'
+  return 'Escolha o tempo determinado'
         
-def seisMeses(update, context, input_text):
-    return Self.filters.check_update(update) 
-    update.message.reply_text(f'Simule um valor a investir todo mês: ')
-    user_message = str(input_text).lower()
-    text = update.Message.Text
-    valorEstimado = ((user_message *1,16) + user_message) * 1,16
-    update.message.reply_text(f'Valor em seis meses: f{valorEstimado}')
+
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
@@ -46,20 +53,20 @@ def handle_message(update, context):
 
 
 
-
-
 app = ApplicationBuilder().token("").build()
 
+
+app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("estimativa", estimativa))
-app.dispatcher.add_handler(CommandHandler('tempInvest', tempInvest))
-app.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+app.add_handler(CommandHandler('tempInvest', tempInvest))
+app.add_handler(CommandHandler('seisMeses', seisMeses))
 app.add_handler(CommandHandler("seisMeses", seisMeses))
 
-app.add_handler(MessageHandler(filters.Text, handle_message, Update))
+app.add_handler(MessageHandler(filters.Text, seisMeses))
 
 app.run_polling(120)
 
-
+app.idle()
 
  
